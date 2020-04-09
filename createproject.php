e9706f35ec490ca2bac5a0d5ae16113ac2fc2425
premnathm <premnathm@dckap.com>
2020-04-09 12:33:49 +0530

    createproject

---
+++ createproject.php
@@ -0,0 +1,32 @@
+<?php
+include('config.php');
+
+function getAllProjects($apiToken,$domain,$url) {
+   $curl = curl_init();
+
+    curl_setopt_array($curl, array(
+      CURLOPT_URL => $url,
+      CURLOPT_RETURNTRANSFER => true,
+      CURLOPT_ENCODING => "",
+      CURLOPT_MAXREDIRS => 10,
+      CURLOPT_TIMEOUT => 0,
+      CURLOPT_FOLLOWLOCATION => true,
+      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+      CURLOPT_CUSTOMREQUEST => "POST",
+      CURLOPT_HTTPHEADER => array(
+                'api-token:'.$apiToken,
+                'domain:'.$domain
+            )
+    ));
+    $response = curl_exec($curl);
+    return $response;
+}
+
+
+$projectname = "Testing Project11223";
+/* Provides the list of projects created for your domain */
+$projecturl  = $url.'project?name='.$projectname;
+$allproject    = getAllProjects($apiToken,$domain,$projecturl);
+echo $allproject;
+
+?>
