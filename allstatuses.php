709999646c8246b99008a2c8951efd40fb003551
premnathm <premnathm@dckap.com>
2020-04-22 17:48:37 +0530

    all statuses

---
+++ allstatuses.php
@@ -0,0 +1,32 @@
+<?php
+include('config.php');
+
+function getAllProjects($apiToken,$domain,$url) {
+    $curl = curl_init();
+    // Set some options - we are passing url and necessary headers
+    curl_setopt_array($curl, array(
+        CURLOPT_RETURNTRANSFER => 1,
+        CURLOPT_URL => $url,
+        CURLOPT_HTTPHEADER => array(
+            'api-token:'.$apiToken,
+            'domain:'.$domain
+        )
+    ));
+    // Send the request & save response to $resp
+    $resp = curl_exec($curl);
+    // Close request to clear up some resources
+    curl_close($curl);
+    // return the response
+    return $resp;
+
+}
+
+/*" List of Status (Pass/Fail) **/
+
+
+/* Count of the total projects available.*/
+$allprojecturl  = $url.'testRuns/getAvailableStatuses';
+$list = getAllProjects($apiToken,$domain,$allprojecturl);
+echo $list;
+
+?>
