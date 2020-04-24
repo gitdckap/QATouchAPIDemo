c5fd5b0bf8afd389e40ccb32b28a4dc6a570db8c
premnathm <premnathm@dckap.com>
2020-04-24 18:57:49 +0530

    defect

---
+++ createdefect.php
@@ -0,0 +1,35 @@
+<?php
+include('config.php');
+
+function getAllProjects($apiToken,$domain,$url) {
+   $curl = curl_init();
+
+    curl_setopt_array($curl, array(
+      CURLOPT_URL => $url,
+      CURLOPT_RETURNTRANSFER => true,
+      CURLOPT_ENCODING => "",
+      CURLOPT_MAXREDIRS => 10,
+      CURLOPT_TIMEOUT => 0,
+      CURLOPT_FOLLOWLOCATION => true,
+      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+      CURLOPT_CUSTOMREQUEST => "POST",
+      CURLOPT_HTTPHEADER => array(
+                'api-token:'.$apiToken,
+                'domain:'.$domain
+            )
+    ));
+    $response = curl_exec($curl);
+    return $response;
+}
+
+
+$projectkey = "BJaX";
+$prioritykey = "High";
+$issueSummary = "Issue is not reproduce";
+
+/* Provides the list of projects created for your domain */
+$projecturl  = $url.'defects?projectKey='.$projectkey.'&priority='.$prioritykey.'&issueSummary='.$issueSummary;
+$allproject    = getAllProjects($apiToken,$domain,$projecturl);
+echo $allproject;
+
+?>
